7612d838bb2275af03a96853c64b2490b9c5e16d
caiolucasb <caio.lucas75@gmail.com>
2021-06-08 14:17:40 -0300

    Modificando o metodo test para fazer validaçao do estudante passado no body

--- Services/StudentService.cs
+++ Services/StudentService.cs
@@ -1,10 +1,15 @@
-using Test.Interfaces;
 using System;
+using Microsoft.AspNetCore.Mvc;
+using Test.Models;
+using Test.Interfaces;
+
 
 namespace Test.Services{
     public class StudentService : IStudentService{
-        public void Test(){
-            System.Console.WriteLine("UUUU");
+        public bool Test(Student student){
+            if(student.age < 6 || student.age > 18 || student.name == null|| student.name == "" || student.surName == null || student.surName == "")
+                return true;
+            return false;
         }
         public void Test2(){
             System.Console.WriteLine("AAAA");
